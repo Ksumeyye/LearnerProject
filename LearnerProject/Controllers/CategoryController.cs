ec41f177d17d22c068f91c06f1285103cb57384d
ksumeyye <sumeyyeekaya@outlook.com>
2024-07-03 00:18:58 +0300

    Add project files.

---
+++ LearnerProject/Controllers/CategoryController.cs
@@ -0,0 +1,59 @@
+﻿using LearnerProject.Models;
+using LearnerProject.Models.Entities;
+using LearnerProject.Models.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LearnerProject.Controllers
+{
+    public class CategoryController : Controller
+    {
+        LearnerContext context=new LearnerContext();
+        public ActionResult Index()
+        {
+            var values = context.Categories.Where(x => x.Status == true).ToList();
+            return View(values);
+        }
+        public ActionResult DeleteCategory(int id)
+        {
+            var value = context.Categories.Find(id);
+            value.Status = false;
+            context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public ActionResult AddCategory()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddCategory(Category category)
+        {
+            category.Status = true;
+            context.Categories.Add(category);
+            context.SaveChanges();
+            return RedirectToAction(nameof(Index));
+        }
+        [HttpGet]
+        public ActionResult UpdateCategory(int id)
+        {
+            var value = context.Categories.Find(id);  // Categories, veritabanındaki tablonun ismi oluyor
+            return View(value);
+        }
+
+        [HttpPost]
+        public ActionResult UpdateCategory(Category category) //Category- sınıfımızın ismi, category- parametre olmakta
+        {
+            var value = context.Categories.Find(category.CategoryId);
+            value.CategoryName = category.CategoryName;
+            value.Icon = category.Icon;
+            value.Status = true;
+            context.SaveChanges();
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
---
+++ LearnerProject/Controllers/TeacherCourseController.cs
@@ -0,0 +1,55 @@
+﻿using LearnerProject.Models.Context;
+using LearnerProject.Models.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LearnerProject.Controllers
+{
+    public class TeacherCourseController : Controller
+    {
+        // GET: TeacherCourse
+        LearnerContext context = new LearnerContext();
+
+        public ActionResult Index()
+        {
+            string name = Session["teacherName"].ToString();
+            var values = context.Courses.Where(x => x.Teacher.NameSurname == name).ToList();
+            return View(values);
+        }
+
+        public ActionResult DeleteCourse(int id)
+        {
+            var values = context.Courses.Find(id);
+            context.Courses.Remove(values);
+            context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+
+        [HttpGet]
+        public ActionResult AddCourse()
+        {
+            List<SelectListItem> category = (from x in context.Categories.Where(x => x.Status == true).ToList()
+                                             select new SelectListItem
+                                             {
+                                                 Text = x.CategoryName,
+                                                 Value = x.CategoryId.ToString()
+                                             }).ToList();
+            ViewBag.category = category;
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddCourse(Course course)
+        {
+            string name = Session["teacherName"].ToString();  //Loginle giriş yapıyoruz,giriş yaptıktan sonra cookiye yani sessione bir teacher name ataması yapıyoruz. Giriş yapmış olduğumuz süre boyunca session da bir değer tutuyor. Eğitmen olarak giriş yaptığımız için yeni kurs eklerken view tarafında eğitmeni seçmemize gerek kalmıyor. Bu yuzden teacher ıd parametresine atama yapıyoruz. Teacher tabosunda eğitmenin id sini seçip bu kısma atıyoruz. Böylelikle teacher id seçimi controller tarafında yapılır.
+            course.TeacherId = context.Teachers.Where(x => x.NameSurname == name).Select(x => x.TeacherId).FirstOrDefault();
+            context.Courses.Add(course);
+            context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
