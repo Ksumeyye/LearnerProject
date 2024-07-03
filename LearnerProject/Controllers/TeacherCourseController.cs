733633801b5326e8029d6ba4f25188336688daae
ksumeyye <sumeyyeekaya@outlook.com>
2024-07-03 23:57:08 +0300

    UI kısmını ekledim

--- LearnerProject/Controllers/TeacherCourseController.cs
+++ LearnerProject/Controllers/TeacherCourseController.cs
@@ -51,5 +51,32 @@ namespace LearnerProject.Controllers
             context.SaveChanges();
             return RedirectToAction("Index");
         }
+        [HttpGet]
+        public ActionResult UpdateCourse(int id)
+        {
+            List<SelectListItem> category = (from x in context.Categories.Where(x => x.Status == true).ToList()
+                                             select new SelectListItem
+                                             {
+                                                 Text = x.CategoryName,
+                                                 Value = x.CategoryId.ToString()
+                                             }).ToList();
+            ViewBag.category = category;
+            var values = context.Courses.Find(id);
+            return View(values);
+        }
+        [HttpPost]
+        public ActionResult UpdateCourse(Course course)
+        {
+            var values = context.Courses.Find(course.CourseId);
+            string name = Session["teacherName"].ToString();  //Loginle giriş yapıyoruz,giriş yaptıktan sonra cookiye yani sessione bir teacher name ataması yapıyoruz. Giriş yapmış olduğumuz süre boyunca session da bir değer tutuyor. Eğitmen olarak giriş yaptığımız için yeni kurs eklerken view tarafında eğitmeni seçmemize gerek kalmıyor. Bu yuzden teacher ıd parametresine atama yapıyoruz. Teacher tabosunda eğitmenin id sini seçip bu kısma atıyoruz. Böylelikle teacher id seçimi controller tarafında yapılır.
+            values.TeacherId = context.Teachers.Where(x => x.NameSurname == name).Select(x => x.TeacherId).FirstOrDefault();
+            values.CourseName= course.CourseName;
+            values.CategoryId= course.CategoryId;
+            values.Price=course.Price;
+            values.Description= course.Description;
+            values.ImageUrl=course.ImageUrl;
+            context.SaveChanges();
+            return RedirectToAction("Index");
+        }
     }
 }
