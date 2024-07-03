179674965c4e997aaea57f67df63082d8b009f49
ksumeyye <sumeyyeekaya@outlook.com>
2024-07-04 01:38:38 +0300

    UI Kısmında değişiklik yaptım.

--- LearnerProject/Controllers/DefaultController.cs
+++ LearnerProject/Controllers/DefaultController.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using LearnerProject.Models;
+using LearnerProject.Models.Context;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -8,10 +11,15 @@ namespace LearnerProject.Controllers
 {
     public class DefaultController : Controller
     {
-        // GET: Default
+       LearnerContext context=new LearnerContext();
         public ActionResult Index()
         {
             return View();
+        }
+        public PartialViewResult DefaultCoursePartial()
+        {
+            var values = context.Courses.Include(x=>x.Reviews).OrderByDescending(x=>x.CourseId).Take(3).ToList(); //Kurs Id'e göre sondan 3 tanesini al ve listele
+            return PartialView(values);
         }
     }
 }
