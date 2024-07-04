f890cb0fb6d91d8e9fcc77822b59209486a12de8
ksumeyye <sumeyyeekaya@outlook.com>
2024-07-05 00:47:42 +0300

    Öğrenci girişi ve kayıt olmayı sağladım

--- LearnerProject/Controllers/CourseRegisterController.cs
+++ LearnerProject/Controllers/CourseRegisterController.cs
@@ -14,11 +14,23 @@ namespace LearnerProject.Controllers
         [HttpGet]
         public ActionResult Index()
         {
+            var courseList = context.Courses.ToList();
+            List<SelectListItem> courses = (from x in courseList select new SelectListItem
+            {
+                Text = x.CourseName,
+                Value = x.CourseId.ToString()
+            }).ToList();
+            ViewBag.course = courses;
             return View();
         }
         [HttpPost]
         public ActionResult Index(CourseRegister courseRegister) 
         {
+            string student = Session["studentName"].ToString();
+            courseRegister.StudentId=context.Students.Where(x=>x.NameSurname==student).Select(x=>x.StudentId).FirstOrDefault();
+            context.CourseRegisters.Add(courseRegister);
+
+
             return View();
         }
     }
